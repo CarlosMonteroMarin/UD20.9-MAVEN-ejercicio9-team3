822b1287638164839771487e6c06f1198f4ae27f
Carlos <CarlosMonteroMarin@carlosmonteromarin@gmail.com>
2022-10-27 10:51:28 +0200

    Estructura básica, con java 1.8

---
+++ TA20.9-Maven/src/main/java/TA20/_Maven/TA20/_Maven/App.java
@@ -0,0 +1,13 @@
+package TA20._Maven.TA20._Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
